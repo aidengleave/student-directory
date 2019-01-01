3bdd2f7dfa5a28b24461a544f10a2f6e7a5daacc
Aiden <aidengleave@gmail.com>
2019-01-01 13:21:31 +0000

    refactor of print method and center formatting

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(100)
+  puts "To finish, just hit return twice".center(100)
   # create an empty array
   students = []
   # get the first name
@@ -8,15 +8,15 @@ def input_students
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    puts "Please enter student's age"
+    puts "Please enter student's age".center(100)
     age = gets.chomp
-    puts "Please enter student's height"
+    puts "Please enter student's height in cm".center(100)
     height = gets.chomp
-    puts "Please enter student's hobby"
+    puts "Please enter student's hobby".center(100)
     hobby = gets.chomp
     students << {name: name, cohort: :November, age: age, 
     height: height, hobby: hobby}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students".center(100)
     # get another name from the user
     name = gets.chomp.capitalize
   end
@@ -25,40 +25,25 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
 end
 
-def print(students)
+def print(students) 
+  #Prints students by first initial if their name is less than 12 characters
   acc = 0
+  #puts "Return names by first initial"
+  #initial = gets.chomp
   while acc < students.count
-    puts "#{acc + 1}: #{students[acc][:name]} 
-    (#{students[acc][:cohort]} cohort, 
-    Age: #{students[acc][:age]}, 
-    Height: #{students[acc][:height]}, 
-    Hobby: #{students[acc][:hobby]})"
+    #if students[acc][:name][0] == initial.upcase && students[acc][:name].length <= 12  
+    puts "#{acc + 1}: #{students[acc][:name]}, #{students[acc][:cohort]} cohort, Age: #{students[acc][:age]}, Height: #{students[acc][:height]} cm, Hobby: #{students[acc][:hobby]}".center(100)
+    #end
     acc += 1
   end  
 end
 
-def print_by_initial(students)
-  puts "Return names by first initial"
-  initial = gets.chomp
-  students.each_with_index {|student, index|
-  if student[:name][0] == initial.upcase  
-    puts "#{index + 1}: #{student[:name]} (#{student[:cohort]} cohort)"
-  end
-}
-end
-def print_if_less_than_12(students)
-  students.each_with_index {|student, index|
-    if student[:name].length <= 12 
-      puts "#{index + 1}: #{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  }
-end
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students."
+  puts "Overall, we have #{students.count} great students.".center(100)
 end
 # nothing happens until we call the methods
 students = input_students
