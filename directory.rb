8add9bd3fdf1873c0586be51e7f0c5f773d215f5
Aiden <aidengleave@gmail.com>
2019-01-04 13:48:50 +0000

    Step 9: Added Interactive Menu and refactor

--- directory.rb
+++ directory.rb
@@ -1,53 +1,77 @@
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't understand, please try again"
+    end
+  end
+end
+
+
 def input_students
-  puts "Please enter the names of the students".center(100)
-  puts "To finish, just hit return twice".center(100)
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
   # create an empty array
   students = []
   cohorts = [:November, :December, :January, :February, :March]
   # get the first name
   name = gets.chop.capitalize
-  #while the name is not empty, repeat this code  
-    if !name.empty? 
-      puts "Enter student's cohort (November to March)".center(100)
+  #while the name is not empty, repeat this code
+   
+  while !name.empty? do
+    puts "Enter student's cohort (November to March)"
+    cohort = gets.chop.capitalize
+    cohort = cohorts.include?(cohort.to_sym) ? cohort : :undef
+        
+    while cohort == :undef do
+      puts "Please check your input for mistakes (November to March)"
       cohort = gets.chop.capitalize
       cohort = cohorts.include?(cohort.to_sym) ? cohort : :undef
-      #if cohort == "Unknown"
-        while cohort == :undef do
-          puts "Please check your input for mistakes (November to March)".center(100)
-          cohort = gets.chop.capitalize
-          cohort = cohorts.include?(cohort.to_sym) ? cohort : :undef
-        end
-      #add the student hash to the array
-      puts "Please enter student's age".center(100)
-      age = gets.chop
-      puts "Please enter student's height in cm".center(100)
-      height = gets.chop
-      puts "Please enter student's hobby".center(100)
-      hobby = gets.chop
-      students << {name: name, cohort: cohort, age: age, 
-      height: height, hobby: hobby}
-    
-      if students.count == 1
-        puts "Now we have #{students.count} student".center(100)
-      else
-        puts "Now we have #{students.count} students".center(100)
-      end
-
-      puts "Please enter another name or press return".center(100)
-      # get another name from the user
-      name = gets.chop.capitalize
-    
-    else
-      print_footer(students)
-      exit
     end
+
+    puts "Please enter student's age"
+    age = gets.chop
+    puts "Please enter student's height in cm"
+    height = gets.chop
+    puts "Please enter student's hobby"
+    hobby = gets.chop
+
+    #add the student hash to the array
+
+    students << {name: name, cohort: :november, age: age, 
+    height: height, hobby: hobby}
+    
+    if students.count == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+    # get another name from the user
+    name = gets.chop.capitalize
+  end
   # return to the array of students
   students
 end
 
 def print(students)  
   acc = 0
-  puts "Please enter first letter of names to print them".center(100)
+  puts "Please enter first letter of names to print them"
   initial = gets.chop 
   print_header
   while acc < students.count
@@ -73,6 +97,4 @@ def print_footer(students)
   end
 end
 # nothing happens until we call the methods
-students = input_students
-print(students)
-print_footer(students)
+interactive_menu
