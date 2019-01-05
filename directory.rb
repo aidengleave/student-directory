e0ec17dd263ec1119036aaf358833b65ddf2e089
Aiden <aidengleave@gmail.com>
2019-01-05 15:36:08 +0000

    Step 14.1: DRY students_hash_to_array

--- directory.rb
+++ directory.rb
@@ -1,67 +1,148 @@
+@students = []
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    @students = input_students
+  when "2"
+    print_header
+    print_student_list
+    print_footer
+  when "3"
+    save_students
+    "puts student"
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+    puts "I don't understand, please try again"
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
+  # create an empty array
   cohorts = [:November, :December, :January, :February, :March]
-  name = gets.chop.capitalize
-     
+  # get the first name
+  name = STDIN.gets.chop.capitalize
+  #while the name is not empty, repeat this code
+   
   while !name.empty? do
     puts "Enter student's cohort (November to March)"
-    cohort = gets.chop.capitalize
+    cohort = STDIN.gets.chop.capitalize
     cohort = cohorts.include?(cohort.to_sym) ? cohort : :undef
-          
+        
     while cohort == :undef do
       puts "Please check your input for mistakes (November to March)"
-      cohort = gets.chop.capitalize
+      cohort = STDIN.gets.chop.capitalize
       cohort = cohorts.include?(cohort.to_sym) ? cohort : :undef
     end
-    puts "Enter student's age"
-    age = gets.chomp
-    puts "Enter student's favourite hobby"
-    hobby = gets.chomp
-  
+
+    puts "Please enter student's age"
+    age = STDIN.gets.chop
+    puts "Please enter student's hobby"
+    hobby = STDIN.gets.chop
+
     #add the student hash to the array
-  
-    students << {name: name, cohort: cohort, age: age, hobby: hobby}
-      
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+
+    students_hash_to_array(name, cohort, age, hobby)
+    
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
-      # get another name from the user
-      name = gets.chop.capitalize
+    # get another name from the user
+    name = STDIN.gets.chop.capitalize
   end
   # return to the array of students
-  students
+  @students
 end
-  
-def print_by_month(students)
-  puts "enter cohort month to print students"
-  month = gets.chomp.capitalize
-  
-  print_header
-  puts "#{month} Cohort".center(100)
-  
-  students.map.with_index { |student, index| if student[:cohort] == month 
-    puts"#{index + 1}: #{student[:name]}".center(100),
-    "They are #{student[:age]} years old, and their favourite hobby is #{student[:hobby]}.".center(100) end }
+
+def students_hash_to_array(name, cohort, age, hobby)
+  @students << {name: name, cohort: cohort, 
+  age: age, hobby: hobby}
+end 
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:age], student[:hobby]]
+    csv_line =student_data.join(",")
+    file.puts csv_line
+  end
+  file.close 
+  puts "Students saved to file"
 end
-  
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort, age, height, hobby = line.chomp.split(",")
+  students_hash_to_array(name, cohort.to_sym, age, hobby)
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}."
+  else
+    puts "Sorry #{filename} doesn't exist."
+    exit
+  end
+end
+
+def print_student_list 
+  acc = 0
+  while acc < @students.count
+    puts "#{acc + 1}: #{@students[acc][:name]}, #{@students[acc][:cohort]} cohort".center(100) 
+    puts "Age: #{@students[acc][:age]}, Height: #{@students[acc][:height]} cm, Hobby: #{@students[acc][:hobby]}.".center(100)
+    puts "---".center(100)
+    acc += 1 
+  end
+end
+
 def print_header
   puts "The students of Villains Academy".center(100)
   puts "-------------".center(100)
 end
-  
-def print_footer(students)
-  if students.count == 1
-    puts "Overall, we have #{students.count} great student."
+
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student.".center(100)
   else
-    puts "Overall, we have #{students.count} great students."
+    puts "Overall, we have #{@students.count} great students.".center(100)
   end
 end
-
-# nothing happens until we call the method
-students = input_students
-print_by_month(students)
-print_footer(students)
+# nothing happens until we call the methods
+try_load_students
+interactive_menu
