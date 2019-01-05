fa5097a5420c74aa126f216f417f4001103455ea
Aiden <aidengleave@gmail.com>
2019-01-05 13:26:30 +0000

    Step 8.8: Refactored to use .map to gain better understanding

--- directory.rb
+++ directory.rb
@@ -1,12 +1,9 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
   students = []
   cohorts = [:November, :December, :January, :February, :March]
-  # get the first name
   name = gets.chop.capitalize
-  #while the name is not empty, repeat this code
      
   while !name.empty? do
     puts "Enter student's cohort (November to March)"
@@ -18,18 +15,14 @@ def input_students
       cohort = gets.chop.capitalize
       cohort = cohorts.include?(cohort.to_sym) ? cohort : :undef
     end
-  
-    puts "Please enter student's age"
-    age = gets.chop
-    puts "Please enter student's height in cm"
-    height = gets.chop
-    puts "Please enter student's hobby"
-    hobby = gets.chop
+    puts "Enter student's age"
+    age = gets.chomp
+    puts "Enter student's favourite hobby"
+    hobby = gets.chomp
   
     #add the student hash to the array
   
-    students << {name: name, cohort: cohort, age: age, 
-    height: height, hobby: hobby}
+    students << {name: name, cohort: cohort, age: age, hobby: hobby}
       
     if students.count == 1
       puts "Now we have #{students.count} student"
@@ -43,28 +36,32 @@ def input_students
   students
 end
   
-def print(students)
-  students.sort_by! {|student| student[:cohort]}
-    students.each_with_index {|student, index| 
-    puts "#{student[:cohort]}".center(100).center(100)
-    puts"#{index + 1}: #{student[:name]}, Age: #{student[:age]}".center(100)
-    puts "Height: #{student[:height]}, Hobby: #{student[:hobby]}".center(100)}
+def print_by_month(students)
+  puts "enter cohort month to print students"
+  month = gets.chomp.capitalize
+  
+  print_header
+  puts "#{month} Cohort".center(100)
+  
+  students.map.with_index { |student, index| if student[:cohort] == month 
+    puts"#{index + 1}: #{student[:name]}".center(100),
+    "They are #{student[:age]} years old, and their favourite hobby is #{student[:hobby]}.".center(100) end }
 end
   
-  def print_header
-    puts "The students of Villains Academy".center(100)
-    puts "-------------".center(100)
-  end
+def print_header
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
+end
   
-  def print_footer(students)
-    if students.count == 1
-      puts "Overall, we have #{students.count} great student.".center(100)
-    else
-      puts "Overall, we have #{students.count} great students.".center(100)
-    end
+def print_footer(students)
+  if students.count == 1
+    puts "Overall, we have #{students.count} great student."
+  else
+    puts "Overall, we have #{students.count} great students."
   end
-  # nothing happens until we call the method
-  students = input_students
-  print_header
-  print(students)
-  print_footer(students)
+end
+
+# nothing happens until we call the method
+students = input_students
+print_by_month(students)
+print_footer(students)
