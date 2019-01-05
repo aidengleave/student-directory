312f0c9d77fbb00cc03fbbf2a0ce3077d1ad4853
Aiden <aidengleave@gmail.com>
2019-01-05 20:47:14 +0000

    Step 14.3+4: Additional refactoring and added Save/Load feedback

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 @students = []
+@cohorts = [:November, :December, :January, :February, :March]
 
 def interactive_menu
   loop do
@@ -44,41 +45,27 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  cohorts = [:November, :December, :January, :February, :March]
-  # get the first name
   name = STDIN.gets.chop.capitalize
   #while the name is not empty, repeat this code
-   
   while !name.empty? do
     puts "Enter student's cohort (November to March)"
     cohort = STDIN.gets.chop.capitalize
-    cohort = cohorts.include?(cohort.to_sym) ? cohort : :undef
-        
+    cohort = @cohorts.include?(cohort.to_sym) ? cohort : :undef
     while cohort == :undef do
       puts "Please check your input for mistakes (November to March)"
       cohort = STDIN.gets.chop.capitalize
       cohort = cohorts.include?(cohort.to_sym) ? cohort : :undef
     end
-
     puts "Please enter student's age"
     age = STDIN.gets.chop
     puts "Please enter student's hobby"
     hobby = STDIN.gets.chop
-
     #add the student hash to the array
-
     students_hash_to_array(name, cohort, age, hobby)
-    
-    if @students.count == 1
-      puts "Now we have #{@students.count} student"
-    else
-      puts "Now we have #{@students.count} students"
-    end
-    # get another name from the user
+    puts "Now we have #{@students.count} student#{plural}"
+    puts "Enter another student or press return for menu"
     name = STDIN.gets.chop.capitalize
   end
-  # return to the array of students
   @students
 end
 
@@ -91,22 +78,21 @@ def save_students
   # open the file for writing
   file = File.open("students.csv", "w")
   # iterate over the array of students
-  @students.each do |student|
+  @students.each {|student|
     student_data = [student[:name], student[:cohort], student[:age], student[:hobby]]
     csv_line =student_data.join(",")
-    file.puts csv_line
-  end
+    file.puts csv_line}
   file.close 
-  puts "Students saved to file"
+  puts "Students saved to file".center(100)
 end
 
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
-  file.readlines.each do |line|
+  file.readlines.each {|line|
   name, cohort, age, height, hobby = line.chomp.split(",")
-  students_hash_to_array(name, cohort.to_sym, age, hobby)
-  end
+  students_hash_to_array(name, cohort.to_sym, age, hobby)}
   file.close
+  puts "Students loaded from file".center(100)
 end
 
 def try_load_students
@@ -125,7 +111,7 @@ def print_student_list
   acc = 0
   while acc < @students.count
     puts "#{acc + 1}: #{@students[acc][:name]}, #{@students[acc][:cohort]} cohort".center(100) 
-    puts "Age: #{@students[acc][:age]}, Height: #{@students[acc][:height]} cm, Hobby: #{@students[acc][:hobby]}.".center(100)
+    puts "Age: #{@students[acc][:age]}, Hobby: #{@students[acc][:hobby]}.".center(100)
     puts "---".center(100)
     acc += 1 
   end
@@ -136,12 +122,16 @@ def print_header
   puts "-------------".center(100)
 end
 
-def print_footer
-  if @students.count == 1
-    puts "Overall, we have #{@students.count} great student.".center(100)
+def plural
+  if @students.count == 1 
+    return ""
   else
-    puts "Overall, we have #{@students.count} great students.".center(100)
-  end
+    return "s"
+  end 
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great student#{plural}.".center(100)
 end
 # nothing happens until we call the methods
 try_load_students
